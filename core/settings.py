893bb1913e28dec5f3b94150cffbb85cf03861f5
Falah <falahmanalodi@gmail.com>
2024-10-03 05:58:46 +0530

    Database setting

--- core/settings.py
+++ core/settings.py
@@ -87,6 +87,11 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
